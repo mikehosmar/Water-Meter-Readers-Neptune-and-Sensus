178087356fe7fb7561f915794a07d89226491c12
Michael Hosmar <mikehosmar@gmail.com>
2024-11-17 23:28:43 -0500

    start meter reading

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,16 @@
 // #include <MycilaWebSerial.h>
 
 AsyncWebServer server(80);
-unsigned long  last_print_time = millis();
+
+#define DEBUG_ESP_PORT WebSerial
+#include <NeptuneProtocol.h>
+int  meter1_clock_pin = SCK;   // black
+int  meter1_read_pin  = MISO;  // red
+int  meter1_relay_pin = 1;
+bool pullup           = false;
+
+NeptuneProtocol          meter1(meter1_clock_pin, meter1_read_pin, meter1_relay_pin, pullup);
+NeptuneProtocol::reading meter1reading;
 
 void setup()
 {
@@ -28,17 +37,7 @@ void setup()
 
   ArduinoOTA
       .onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-        {
-          type = "sketch";
-        }
-        else
-        {  // U_SPIFFS
-          type = "filesystem";
-        }
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
         Serial.println("Start updating " + type);
       })
       .onEnd([]() { Serial.println("\nEnd"); })
@@ -48,43 +47,36 @@ void setup()
       .onError([](ota_error_t error) {
         Serial.printf("Error[%u]: ", error);
         if (error == OTA_AUTH_ERROR)
-        {
           Serial.println("Auth Failed");
-        }
         else if (error == OTA_BEGIN_ERROR)
-        {
           Serial.println("Begin Failed");
-        }
         else if (error == OTA_CONNECT_ERROR)
-        {
           Serial.println("Connect Failed");
-        }
         else if (error == OTA_RECEIVE_ERROR)
-        {
           Serial.println("Receive Failed");
-        }
         else if (error == OTA_END_ERROR)
-        {
           Serial.println("End Failed");
-        }
       });
 
   ArduinoOTA.begin();
 
-  // WebSerial is accessible at "<IP Address>/webserial" in browser
   server.onNotFound([](AsyncWebServerRequest* request) { request->redirect("/webserial"); });
   WebSerial.begin(&server);
-
   server.begin();
+
+  meter1.setup();
 
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
+unsigned long last_print_time = millis();
+unsigned long last_read_time;
+
 void loop()
 {
-  // Print every 2 seconds (non-blocking)
+  // Print every 30 seconds (non-blocking)
   if ((unsigned long)(millis() - last_print_time) > 30000)
   {
     WebSerial.print(F("IP address: "));
@@ -92,6 +84,27 @@ void loop()
     WebSerial.printf("Uptime: %lums\n", millis());
     WebSerial.printf("Free heap: %" PRIu32 "\n", ESP.getFreeHeap());
     last_print_time = millis();
+  }
+
+  // Read meter every 10 seconds
+  if ((unsigned long)(millis() - last_read_time) > 10000)
+  {
+    last_read_time = millis();
+    meter1reading  = {};  // Clear struct
+    meter1.readMeter(&meter1reading);
+    WebSerial.println("Meter 1");
+    WebSerial.print(" Serial Number : ");
+    WebSerial.println(meter1reading.serialNum);
+    WebSerial.print(" Reading Value : ");
+    WebSerial.println(meter1reading.readVal);
+    WebSerial.print(" Software Ver  : ");
+    WebSerial.println(meter1reading.swver);
+    WebSerial.print(" Unknown 1     : ");
+    WebSerial.println(meter1reading.unknown1);
+    WebSerial.print(" Unknown 2     : ");
+    WebSerial.println(meter1reading.unknown2);
+    WebSerial.print(" Unknown 3     : ");
+    WebSerial.println(meter1reading.unknown3);
   }
 
   WebSerial.loop();
